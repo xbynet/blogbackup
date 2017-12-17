824737e9bdd43747d0e329ae4b1c12bd814e5e68
xbynet@outlook.com <xbynet@outlook.com>
2017-12-17 13:41:43 +0800

    1

--- gulpfile - 副本.js
+++ gulpfile - 副本.js
@@ -1,7 +1,35 @@
 var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
-
+/** //压缩 public 目录 css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+});
+// 压缩 public 目录 html
+gulp.task('minify-html', function() {
+  return gulp.src('./public/**/*.html')
+    .pipe(htmlclean())
+    .pipe(htmlmin({
+         removeComments: true,
+         minifyJS: true,
+         minifyCSS: true,
+         minifyURLs: true,
+    }))
+    .pipe(gulp.dest('./public'))
+});
+// 压缩 public/js 目录 js
+gulp.task('minify-js', function() {
+    return gulp.src('./public/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+*/
 
 //压缩图片
 gulp.task('imgmin', () =>{
