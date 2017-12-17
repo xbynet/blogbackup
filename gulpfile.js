2505d76c41fdf6bab56b75f0a13bc703c86f6b37
xbynet@outlook.com <xbynet@outlook.com>
2017-12-17 13:18:32 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
 // 压缩 public 目录 css
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
@@ -27,7 +29,25 @@ gulp.task('minify-js', function() {
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
+//压缩图片
+gulp.task('imgmin', () =>{
+    return gulp.src(['./public/**/*.{png,jpg,jpeg,gif,bmp,ico}'])
+		.pipe(
+			//cache(
+			imagemin({
+			optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+			svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
+			use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
+		})
+		//)
+		)
+        .pipe(gulp.dest('./public'));
+	}
+);
 // 执行 gulp 命令时执行的任务
 gulp.task('default', [
-    'minify-html','minify-css','minify-js'
+    'minify-html','minify-css','minify-js','imgmin'
 ]);
